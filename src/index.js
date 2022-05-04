ae1c8143d54e354e1cd5b3e1e3aa47ec25eb25e3
bimri <bimri@outlook.com>
2022-05-04 21:44:52 +0300

    Adapting Our API

--- src/index.js
+++ src/index.js
@@ -1,38 +1,68 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 
-// Run the server on a port specified in our .env file or port 4000
+// Run our server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
+
+let notes = [
+    { id: '1', content: 'This is a note', author: 'Adam Scott' },
+    { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+    { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
 
 // Construct a schema, using GraphQL's schema language
 const typeDefs = gql`
-    type Query {
-        hello: String
+    type Note {
+    id: ID!
+    content: String!
+    author: String!
+}
+
+type Query {
+    hello: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+}
+
+type Mutation {
+    newNote(content: String!): Note!
     }
 `;
 
 // Provide resolver functions for our schema fields
 const resolvers = {
     Query: {
-        hello: () => 'Hello world!'
-    }
+    hello: () => 'Hello world!',
+    notes: () => notes,
+    note: (parent, args) => {
+        return notes.find(note => note.id === args.id);
+        }
+    },
+
+    Mutation: {
+        newNote: (parent, args) => {
+        let noteValue = {
+            id: String(notes.length + 1),
+            content: args.content,
+            author: 'Adam Scott'
+        };    
+        notes.push(noteValue);
+        return noteValue;
+        }
+    }   
 };
 
 
-async function startApolloServer(typeDefs, resolvers) {
-  // Same ApolloServer initialization as before
-  const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
 
-  // Required logic for integrating with Express
-  await server.start();
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
 
-  const app = express();
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
 
-  // Apollo Server setup
-  server.applyMiddleware({ app, path: '/api'});
-
-  // Modified server startup
-  await new Promise(resolve => app.listen({ port }, resolve));
-  console.log(console.log(
-    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`));
-};
+app.listen({ port }, () =>
+console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+    )
+);
