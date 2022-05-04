f1e3b174aa76bb372dce1673185eccfd6e7d5e72
bimri <bimri@outlook.com>
2022-05-04 18:47:47 +0300

    Extending Port Options

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+ 
 const app = express();
+const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello World Web Server Bimri'));
-app.listen(4000, () => console.log('Listening on port 4000!'));
+app.get('/', (req, res) => res.send('Hello World Bimri'));
+app.listen(port, () => 
+    console.log(`Server running at htpp://localhost:${port}`)
+);
