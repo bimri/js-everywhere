b89a109ebed54b903d8f7b967488137674372fc4
bimri <bimri@outlook.com>
2022-05-04 18:42:28 +0300

    Nodemon install & setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello World Web Server Bimri'));
 app.listen(4000, () => console.log('Listening on port 4000!'));
